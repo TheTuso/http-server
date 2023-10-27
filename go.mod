ff35ee904b22d47504a4c82c7c66e93af72fe8d1
TheTuso <piotrekpasztor@gmail.com>
2023-10-27 19:36:06 +0200

    Fix

--- go.mod
+++ go.mod
@@ -3,8 +3,10 @@ module http-server
 go 1.21
 
 require (
+	github.com/felixge/httpsnoop v1.0.1 // indirect
 	github.com/go-sql-driver/mysql v1.7.1 // indirect
 	github.com/google/uuid v1.3.1 // indirect
+	github.com/gorilla/handlers v1.5.1 // indirect
 	github.com/gorilla/mux v1.8.0 // indirect
 	github.com/joho/godotenv v1.5.1 // indirect
 )
--- main.go
+++ main.go
@@ -2,12 +2,17 @@ package main
 
 import (
 	_ "github.com/go-sql-driver/mysql"
+	"github.com/gorilla/handlers"
 	"http-server/api"
+	"log"
 	"net/http"
+	"os"
 )
 
 func main() {
-	if err := http.ListenAndServe(":8080", api.NewServer()); err != nil {
-		panic(err)
-	}
+	log.Fatal(http.ListenAndServe(":8080",
+		handlers.LoggingHandler(os.Stdout, handlers.CORS(
+			handlers.AllowedMethods([]string{"POST", "GET", "DELETE", "PUT", "OPTIONS"}),
+			handlers.AllowedOrigins([]string{"*"}),
+			handlers.AllowedHeaders([]string{"X-Requested-With"}))(api.NewServer()))))
 }
