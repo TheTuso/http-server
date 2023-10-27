e5e35c48e97e1d30f4a0317547f45eac47b31083
TheTuso <piotrekpasztor@gmail.com>
2023-10-27 19:27:29 +0200

    Fix

--- api/server.go
+++ api/server.go
@@ -37,7 +37,7 @@ func (server *Server) routes() {
 		return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 			// Set CORS headers
 			w.Header().Set("Access-Control-Allow-Origin", "*")
-			w.Header().Set("Access-Control-Allow-Methods", "GET, POST, OPTIONS")
+			w.Header().Set("Access-Control-Allow-Methods", "GET, POST, OPTIONS, DELETE")
 			w.Header().Set("Access-Control-Allow-Headers", "Content-Type")
 
 			// Continue with the request
